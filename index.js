8c7ced94eccf26de9d58b9d251ec549adb845a75
sandraarambulo <arambulosandra5@gmail.com>
2024-05-25 15:14:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+   <title> All About Me </title>
+   <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+   </head>
+   <body> 
+      <div class="card">
+  <div class="card-header">
+    Homepage
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">My BIO</h5>
+    <p class="card-text">Sandra Juan Arambulo</p>
+  </div>
+</div>
+       <div class="container">
+          <div class="row">
+             <div class="col-6">
+               <img src="aa.jpg" class="img-thumbnail" style="width:500px;height:300px;"/> <br>
+               <button class="btn btn-outline-primary" type="button" id="btnLike1"> Like </button>
+        <output id="countLikes1"> 0 </output>
+        <button class="btn btn-outline-primary" type="button" id="btnLike1"> Like </button>
+        <output id="countLikes1"> 0 </output>
+             </div>
+            <div class="col-6">
+              <img src="ss.jpg" class="img-thumbnail" style="width:500px;height:300px;"/> <br>
+              <button class="btn btn-outline-primary" type="button" id="btnLike2"> Like </button>
+       <output id="countLikes2"> 0 </output>
+            </div>
+          </div>
+       </div>
+       <br><br>
+       <div class="card text-center">
+      <div class="card-footer text-body-secondary">
+      Owned By Sandra Arambulo
+    </div>
+    </div>
+
+      <script src="index.js"> </script>
+   </body>
+</html>
