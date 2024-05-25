e9f28a6dfa17335c027b1d8d1b9961dccbff7852
sandraarambulo <arambulosandra5@gmail.com>
2024-05-25 16:21:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
+const btnLike1 = document.getElementById("btnLike1")
+const countLikes1 = document.getElementById("countLikes1")
+const btnLike2 = document.getElementById("btnLike2")
+const countLikes2 = document.getElementById("countLikes2")
 
-<!DOCTYPE html>
-<html lang="en" data-bs-theme="dark">
-   <title> All About Me </title>
-   <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-   </head>
-   <body> 
-      <div class="card">
-  <div class="card-header">
-    Homepage
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">My BIO</h5>
-    <p class="card-text">Sandra Juan Arambulo</p>
-  </div>
-</div>
-       <div class="container">
-          <div class="row">
-             <div class="col-6">
-               <img src="aa.jpg" class="img-thumbnail" style="width:500px;height:300px;"/> <br>
-               <button class="btn btn-outline-primary" type="button" id="btnLike1"> Like </button>
-        <output id="countLikes1"> 0 </output>
-        <button class="btn btn-outline-primary" type="button" id="btnLike1"> Like </button>
-        <output id="countLikes1"> 0 </output>
-             </div>
-            <div class="col-6">
-              <img src="ss.jpg" class="img-thumbnail" style="width:500px;height:300px;"/> <br>
-              <button class="btn btn-outline-primary" type="button" id="btnLike2"> Like </button>
-       <output id="countLikes2"> 0 </output>
-            </div>
-          </div>
-       </div>
-       <br><br>
-       <div class="card text-center">
-      <div class="card-footer text-body-secondary">
-      Owned By Sandra Arambulo
-    </div>
-    </div>
-
-      <script src="index.js"> </script>
-   </body>
-</html>
+function clickLike1(){
+    let totalLikes1 = parseInt(countLikes1.value) + 1
+    countLikes1.textContent = totalLikes1.toString()
+  }
+function clickLike2(){
+    let totalLikes2 = parseInt(countLikes2.value) + 1
+    countLikes2.textContent = totalLikes2.toString()
+}
+btnLike1.addEventListener("click",clickLike1)
+btnLike2.addEventListener("click",clickLike2)
