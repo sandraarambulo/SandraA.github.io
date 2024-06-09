b0ff95b6605424d110d7ac5d6c70b3e8da2ec740
sandraarambulo <arambulosandra5@gmail.com>
2024-06-09 02:12:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const btnLike1 = document.getElementById("btnLike1")
+const btnLike1 = document.getElementById("\uF59D")
 const countLikes1 = document.getElementById("countLikes1")
 const btnLike2 = document.getElementById("btnLike2")
 const countLikes2 = document.getElementById("countLikes2")
